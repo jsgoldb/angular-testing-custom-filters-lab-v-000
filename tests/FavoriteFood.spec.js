ca622af86dce7a4eec63f4a83e2b4971d385bb38
jsgoldb <jgoldb@yahoo.com>
2016-06-03 13:40:46 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,34 @@
 describe('Favorite Food Filter', function () {
 	var $controller;
 
+  var people = [
+  {
+    name: 'Tom',
+    favoriteFood: 'pizza'
+  },
+  {
+    name: 'Tina',
+    favoriteFood: 'bananas'
+  },
+  {
+    name: 'Horatio',
+    favoriteFood: 'apples'
+  },
+  {
+    name: 'Nina',
+    favoriteFood: 'pizza'
+  }
+  ]
+
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function($filter){
+    favoriteFood = $filter('favoriteFood')
+  }));
+
+  it('should select the people whose favorite food matches the filter', function(){
+    expect(favoriteFood(people, 'pizza')).toEqual([{name: 'Tom', favoriteFood: 'pizza'}, {name: 'Nina', favoriteFood: 'pizza'}]);
+  });
 
 	
 });
